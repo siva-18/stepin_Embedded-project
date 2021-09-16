50591a611592e46467caa1ac5ee4caab0b857d33
siva-18 <89982252+siva-18@users.noreply.github.com>
2021-09-16 21:04:27 +0530

    Update activity1.h

--- inc/activity1.h
+++ inc/activity1.h
@@ -10,9 +10,9 @@
  * Macro Definitions
  */
 
-#define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
-#define LED_PORT (PORTD)    /**< LED Port Number */
-#define LED_PIN  (PORTD2)   /**< LED Pin number  */
+#define F_CPU 16000000UL 	/**< clock Frequency of MCU is 16 MHz */
+#define LED_PORT (PORTD)    /**< LED Port */
+#define LED_PIN  (PORTD2)   /**< LED Pin */
 #define BUTTON_SENSOR  (PORTD0)   /**< Port for Button Sensor  */
 #define TEMP_SENSOR  (PORTD1)   /**< Port for Temperature Sensor   */
 
