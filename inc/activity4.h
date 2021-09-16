247282b5b2af50c8a774225846a56755cf377827
siva-18 <89982252+siva-18@users.noreply.github.com>
2021-09-16 21:17:39 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -2,18 +2,15 @@
 #define __ACTIVITY_4_H__
 /**
  * @file activity4.h
- * @brief Header file for activity4
- * @version 0.1
- * @copyright Copyright (c) 2021
- * 
+ * @brief header file for activity_4
  */
 
 /**
- * Macro Definitions
+ * Macro definitions
  */
-#define F_CPU 16000000UL /**< Clock Frequency of MCU is 16 MHz */
+#define F_CPU 16000000UL /**< clock frequency of MCU is 16 MHz */
 #define USART_BAUDRATE 9600 /**< Baud rate for serial communication  */
-#define UBRR_VALUE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1) /**< Formula to calculate UBRR value */
+#define UBRR_VALUE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1) /**< formula to calculate UBRR value */
 
 /**
  * Include files
@@ -26,20 +23,20 @@
  */
 
 /**
- * @brief Setting Baud rate and Enabling Rx and Tx 
+ * @brief Setting Baud rate and enabling Rx and Tx 
  * 
  */
 void USARTInit();
 
 /**
- * @brief Reading data
+ * @brief reading data
  * 
  * @return int The read data
  */
 int USARTRead();
 
 /**
- * @brief Writing data to view in serial monitor
+ * @brief writing data to view in serial monitor
  * 
  * @param[in] temp The ADC value
  */
